124fd38958ed45488567360a44b45b76faeb9dfb
Leandro Szikora <lszikora@est.frba.utn.edu.ar>
2018-09-05 11:50:11 -0300

    Parte 1 terminada y testeada (falta bind)

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,6 +1,39 @@
 class Prueba
 
-  def materia
-    :tadp
+end
+
+class Object
+  # 1era Parte
+  def val(un_obj)
+    Proc.new {|otro_obj| un_obj == otro_obj}
+  end
+
+  def type(un_tipo)
+    Proc.new {|un_obj| un_obj.is_a?(un_tipo)}
+  end
+
+  def list(un_array, match_size=true)
+    Proc.new {|otro_array| match_size ? val(otro_array).call(un_array) : un_array.son_n_iguales?(otro_array)}
+  end
+
+  def entiende_mensaje(sym)
+    self.public_methods.include?(sym)
+  end
+
+  def duck(symbol, *symbols)
+    Proc.new do |un_obj|
+      un_obj.entiende_mensaje(symbol) && (symbols.empty? ? true : symbols.any? {|sym| un_obj.entiende_mensaje(sym)})
+    end
+  end
+
+  # 2da Parte
+  def and(*matchers)
+    
+  end
+end
+
+class Array
+  def son_n_iguales?(otro_array)
+    self == otro_array.first(self.length)
   end
 end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,9 +1,57 @@
-describe Prueba do
-  let(:prueba) { Prueba.new }
+describe Object do
 
-  describe '#materia' do
-    it 'debería pasar este test' do
-      expect(prueba.materia).to be :tadp
+  describe '#Pruebas tp' do
+    it 'paso test de val' do
+      expect(val(5).call(5)).to be true
+      expect(val(5).call('5')).to be false
+      expect(val(5).call(4)).to be false
+    end
+
+    it 'paso test de type' do
+      expect(type(Integer).call(5)).to be true
+      expect(type(Symbol).call('Trust me, sarasa')).to be false
+      expect(type(Symbol).call(:a_true_symbol)).to be true
+    end
+
+    it 'paso test de list' do
+      an_array = [1, 2, 3, 4]
+
+      expect(list([1, 2, 3, 4], true).call(an_array)).to be true
+      expect(list([1, 2, 3, 4], false).call(an_array)).to be true
+
+      expect(list([1, 2, 3], true).call(an_array)).to be false
+      expect(list([1, 2, 3], false).call(an_array)).to be true
+
+      expect(list([2, 1, 3, 4], true).call(an_array)).to be false
+      expect(list([2, 1, 3, 4], true).call(an_array)).to be false
+
+      expect(list([1, 2, 3]).call(an_array)).to be false
+    end
+
+    it 'paso test de duck' do
+
+      psyduck = Object.new
+      def psyduck.cuack
+        'psy..duck?'
+      end
+      def psyduck.fly
+        '(headache)'
+      end
+
+      class Dragon
+        def fly
+          'do some flying'
+        end
+      end
+      a_dragon = Dragon.new
+
+      expect(duck(:cuack, :fly).call(psyduck)).to be true
+      expect(duck(:cuack, :fly).call(a_dragon)).to be false
+      expect(duck(:fly).call(a_dragon)).to be true
+      expect(duck(:to_s).call(Object.new)).to be true
+
     end
   end
+
+
 end
