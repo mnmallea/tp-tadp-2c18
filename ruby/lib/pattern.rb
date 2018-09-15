4fa4bfdbe979c335aa698caea8e397c1925e2823
nachozullo <ignaciozullo@gmail.com>
2018-09-15 02:04:43 -0300

    Terminamos loñ de bind

--- ruby/lib/pattern.rb
+++ ruby/lib/pattern.rb
@@ -1,8 +1,21 @@
 module Pattern
-  def with(un_matcher, *otros_matchers, &bloque)
-    matchers = otros_matchers << un_matcher
-    if self.todos_matchean matchers
-      bloque.call
+  def with(*matchers, &bloque)
+    if self.todos_matchean?(matchers)
+
+      if matchers.any? {|m| type(Symbol).call(m)}
+        symbols = matchers.select {|m| type(Symbol).call(m)}
+
+        context = Contexto.new
+        if type(Array).call(self)
+          symbols.each.with_index {|s,i| context.bind(s, self[i])}
+        else
+          context.bind(symbols[0], self)
+        end
+        context.instance_exec(&bloque)
+      else
+        bloque.call
+      end
+
     end
   end
 
@@ -10,7 +23,15 @@ module Pattern
     bloque.call
   end
 
-  def todos_matchean(unos_matchers)
-    unos_matchers.all? {|matcher| matcher.call self}
+  def todos_matchean?(unos_matchers)
+    unos_matchers.all? { |matcher| matcher.call(self) }
+  end
+end
+
+class Contexto
+  def bind(nombre_variable, objeto = nil )
+    self.class.send(:attr_accessor, nombre_variable)
+    nombre_setter = (nombre_variable.to_s + "=").to_sym
+    send(nombre_setter, objeto)
   end
 end
--- ruby/script.rb
+++ ruby/script.rb
@@ -1,7 +1,22 @@
 require_relative 'lib/metamodelo'
 
 x = 1
-matches? x do
-  with(val(1), type(Integer)){puts x}
-  otherwise{puts "hola"}
+matches?(x) do
+  with(val(1), type(Integer)) { puts x }
+  otherwise { puts "hola" }
 end
+
+=begin
+y = "holita"
+matches?(y) do
+  with(:a_string) { p a_string.length }
+  otherwise {puts "commitea capo"}
+end
+
+z = [4, 8]
+
+matches?(z) do
+  with(:k, :m) {p k + m}
+  otherwise {puts "commitea capo2"}
+end
+=end
